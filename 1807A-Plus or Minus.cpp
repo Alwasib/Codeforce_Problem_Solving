1b9d7f95cee309764274a6906851642a8641d23f
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-07-05 17:53:17 +0600

    Add files via upload

---
+++ 1807A-Plus or Minus.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,a,b,c;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>a>>b>>c;
+        if(a+b==c)
+        {
+            cout<<"+"<<endl;
+        }
+        else if(a-b==c)
+        {
+            cout<<"-"<<endl;
+        }
+    }
+}
