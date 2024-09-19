de814adf50c2ecf8fe1c0f15579a64d1734e6661
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-09-19 20:25:50 +0600

    Add files via upload

---
+++ 1095A-Repeating Cipher.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string s,m;
+    cin>>s;
+    for(int i=0;i<n;i++)
+    {
+        if(i==0)
+        {
+            m=m+s[i];
+        }
+        else if(i==2)
+        {
+            m=m+s[i];
+        }
+        else if(i==5)
+        {
+            m=m+s[i];
+        }
+        else if(i==9)
+        {
+            m=m+s[i];
+        }
+        else if(i==14)
+        {
+            m=m+s[i];
+        }
+         else if(i==20)
+        {
+            m=m+s[i];
+        }
+         else if(i==27)
+        {
+            m=m+s[i];
+        }
+        else if(i==35)
+        {
+            m=m+s[i];
+        }
+         else if(i==45)
+        {
+            m=m+s[i];
+        }
+        else if(i==53)
+        {
+            m=m+s[i];
+        }
+    }
+    cout<<m;
+}
