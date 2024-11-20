d9451a12dce7ca148cb55db9f752baed732f8efe
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-11-20 23:02:18 +0600

    Add files via upload

---
+++ mock2024F.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==48 ||n==50||n==49)
+    {
+        cout<<"Probin"<<endl;
+    }
+    else if(n==51 || n==53||n==52)
+    {
+        cout<<"Nobin"<<endl;
+    }
+    else{
+        cout<<"Bohiragoto"<<endl;
+    }
+}
+
