7d537a2e7a5aae67ec22b0c8b9546cbc98118cbc
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-12-01 23:06:39 +0600

    Add files via upload

---
+++ 1102A- Integer Sequence Dividing.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a;
+    a=n*(n+1)/2;
+    if(a%2==0)
+    {
+        cout<<"0"<<endl;
+    }
+    else
+    {
+        cout<<1<<endl;
+    }
+}
