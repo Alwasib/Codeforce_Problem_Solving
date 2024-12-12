68d7e4d9be1da1e7be3188d6ccc8e53c368077ce
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-12-12 20:10:49 +0600

    Add files via upload

---
+++ 2001A-Make all Equal.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,c=0,mx=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+            mx=max(mx,c);
+            c=0;
+        }
+        cout<<abs(n-mx)<<endl;
+    }
+}
