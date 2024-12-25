925638b45e9c09815a8d54fbd0cc0ecf0865d57d
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-12-25 16:10:08 +0600

    Add files via upload

---
+++ 1899A-A Game with Integer.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if((n-1)%3==0)
+        {
+            cout<<"First"<<endl;
+        }
+        else if((n+1)%3==0)
+        {
+            cout<<"First"<<endl;
+        }
+        else{
+            cout<<"Second"<<endl;
+        }
+    }
+}
