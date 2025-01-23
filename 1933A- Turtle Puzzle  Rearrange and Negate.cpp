4f65e806e8f8144e1ba81e7559822559119bef10
alwasib <122206303+Alwasib@users.noreply.github.com>
2025-01-23 11:27:04 +0600

    Add files via upload

---
+++ 1933A- Turtle Puzzle  Rearrange and Negate.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,sum=0,b;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+         for(int i=0;i<n;i++)
+        {
+            b=abs(a[i]);
+            sum=sum=sum+b;
+            b=0;
+        }
+        cout<<sum<<endl;
+    }
+}
